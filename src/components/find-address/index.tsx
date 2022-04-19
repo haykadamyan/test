372286597b704410540cf8b0172bd9c756c9674a
ArtakoM <artakom98@gmail.com>
2022-04-19 17:45:40 +0400

    working on top section (wip)

--- src/components/find-address/index.tsx
+++ src/components/find-address/index.tsx
@@ -1,5 +1,7 @@
 import { FunctionComponent } from 'react';
 
+import { SearchIcon } from '@heroicons/react/outline'
+
 import styles from './styles.module.scss';
 
 type FindAddressProps = {};
@@ -7,8 +9,18 @@ type FindAddressProps = {};
 const FindAddress: FunctionComponent<FindAddressProps> = () => {
   return (
     <section className={styles.wrapper}>
-        <div>asdasd</div>
-        <div>asdasdsadasd</div>
+      <div className={styles['inner-wrapper']}>
+        <h2 className={styles.heading}>
+          Enter your address to find out if
+          your property is at risk from fraud
+        </h2>
+        <div className={styles['search-wrapper']}>
+          <label htmlFor="search">
+            <SearchIcon className={styles.icon} />
+          </label>
+          <input type="text" name='search' id='search' />
+        </div>
+      </div>
     </section>
   )
 }
