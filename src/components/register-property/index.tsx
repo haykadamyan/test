e051f56cffd51391f29446bb7e3223981d675833
ArtakoM <artakom98@gmail.com>
2022-04-21 20:54:15 +0400

    reg property

--- src/components/register-property/index.tsx
+++ src/components/register-property/index.tsx
@@ -7,7 +7,10 @@ function RegisterProperty() {
     <div className={styles.wrapper}>
       <div className={styles['inner-wrapper']}>
         <div className={styles.content}>
-          <p className="text-3xl font-medium">
+          <div className={styles['icon-wrapper']}>
+            <img src="/logo-icon.png" alt="logo-icon" />
+          </div>
+          <p className={styles.text}>
             Relax and enjoy your time with those you love and take comfort knowing we have you protected
           </p>
           <button className="mt-4 px-8 py-4 rounded-full text-body1 font-medium bg-white/30 text-white">
